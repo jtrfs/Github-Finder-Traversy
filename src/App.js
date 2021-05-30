c93a253e1f8cdaa78edd3d3ae8a4357b48afd014
Jan Varmuza <jan.varmuza@gmail.com>
2021-05-30 21:19:14 +0200

    just playing with git

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
     alert: null
   };
 
-  // this is going to be refactored with hooks and context api
+  // it is going to be refactored with hooks and context api
 
   // SEARCH FOR & GET GH USERS
   searchUsers = async text => {
